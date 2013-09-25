b1491820764b1ca52702e105ddb3911f6e5cd846
xulegaspi <xulegaspi@gmail.com>
2013-09-25 16:45:34 +0200

    Finnished django tutorial part 3. New url's added

--- prueba/urls.py
+++ prueba/urls.py
@@ -15,5 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^polls/', include('prueba_app.urls'))
+    url(r'^polls/', include('prueba_app.urls', namespace="polls"))
 )
